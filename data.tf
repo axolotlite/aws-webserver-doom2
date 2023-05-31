69ed4a6663e6a65cb205a90d65fec8db1b1180d7
Ahmed Said <ahmed.el.sayeed1914@gmail.com>
2023-06-01 02:25:33 +0300

    changed ami to ubuntu 20

--- data.tf
+++ data.tf
@@ -4,15 +4,8 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20220912"]
+    values = ["ami-0261755bbcb8c4a84"]
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"]
 }
 data "template_file" "npm_app" {
   template = "${file("scripts/npm_app.tpl")}"
